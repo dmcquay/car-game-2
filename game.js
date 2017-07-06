4dbce0d0f15db03ceb3679db89c3ba0d4bdfbf3f
Dustin McQuay <dmcquay@gmail.com>
2017-07-05 23:18:35 -0600

    lane changing

--- game.js
+++ game.js
@@ -11,7 +11,8 @@ carImg.src = 'red-car-top-view-hi.png'
 const carHeight = 200
 const carWidth = 100
 
-const pixelsPerSecond = 500
+const pixelsPerSecond = 300
+const laneChangePixelsPerSecond = 500
 const startTime = new Date().getTime()
 
 const laneWidth = carWidth * 1.5
@@ -19,10 +20,37 @@ const laneCount = 3
 const laneLineWidth = 10
 const lanesX = (canvas.width / 2) - (laneWidth * laneCount / 2)
 const lanesWidth = laneWidth * laneCount
+let currentLane = 0
 
+const defaultCarX = (canvas.width / 2) - (carWidth / 2)
 const car = {
-    x: (canvas.width / 2) - (carWidth / 2),
+    x: defaultCarX,
     y: ((canvas.height / 4) * 3) - (carHeight / 2)
+}
+
+const leftArrowKeyCode = 37
+const rightArrowKeyCode = 39
+
+window.addEventListener('keyup', evt => {
+    if (evt.keyCode === leftArrowKeyCode) {
+        currentLane = Math.max(currentLane - 1, (laneCount - 1) / 2 * -1)
+    } else if (evt.keyCode === rightArrowKeyCode) {
+        currentLane = Math.min(currentLane + 1, (laneCount - 1) / 2)
+    }
+})
+
+let lastUpdateTime = new Date().getTime()
+function update() {
+    const timeSinceLastUpdate = new Date().getTime() - lastUpdateTime
+
+    const targetCarX = defaultCarX + (currentLane * laneWidth)
+    const maxLaneChangeMovementPx = (timeSinceLastUpdate / 1000) * laneChangePixelsPerSecond
+    const desiredLaneChangeMovementPx = Math.abs(car.x - targetCarX)
+    const actualLaneChangeMovementPx = Math.min(desiredLaneChangeMovementPx, maxLaneChangeMovementPx)
+    if (targetCarX < car.x) car.x -= actualLaneChangeMovementPx
+    else car.x += actualLaneChangeMovementPx
+
+    lastUpdateTime = new Date().getTime()
 }
 
 function render() {
@@ -65,10 +93,12 @@ function render() {
         ctx.stroke()
     }
 
+    // draw car
     if (carImgReady) ctx.drawImage(carImg, car.x, car.y, carWidth, carHeight)
 }
 
 function main() {
+    update()
     render()
     requestAnimationFrame(main)
 }
